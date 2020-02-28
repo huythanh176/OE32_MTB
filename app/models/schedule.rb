2493975523b0655b70d74d28af96e040aef52562
huythanh <huythanh1706@gmail.com>
2020-02-28 15:03:43 +0700

    show schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,5 @@ class Schedule < ApplicationRecord
   belongs_to :movie
   belongs_to :room
   has_many :bookings, dependent: :destroy
+  scope :get_schedule_by_day, -> (day){where "start_at like ?", day}
 end
